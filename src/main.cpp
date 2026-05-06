eac880674518ba622a6ad3a37e3c09602c46d21b
PEDRO LEONEL DE LORENA <53955089886@info.local>
2026-05-06 16:15:31 -0300

    Implantado as funções de WiFi, Debug, MQTT e o secrets

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,166 @@
+/**
+ *  Nome: Pedro Leonel de Lorena
+ *  Descrição: Este projeto realiza a conexao do esp com o wifi e o protocolo mqtt
+ *  Projeto: conexao MQTT
+ *  Data: 28/04
+ *  Versão: 1.0
+ */
+
 #include <Arduino.h>
+#include "WiFiManager.h"
+#include "MqttManager.h"
+#include "DebugManager.h"
+#include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int PINO_LED_RGB = 48;
+const int QUANTIDADE_LEDS = 1;
+const int PINO_LAMPADA = 15;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const char TOPICO_COMANDO[] = "senai134/pedroleonel/esp32/comando";
+
+bool lampada = false;
+
+Adafruit_NeoPixel ledRGB(
+    QUANTIDADE_LEDS,
+    PINO_LED_RGB,
+    NEO_GRB + NEO_KHZ800 // constante de configuração
+);
+
+void tratarMensagemRecebida(const char *topico, const String &mensagem);
+void configurarLedRGB();
+void alterarCorLedRGB(int vermelho, int verde, int azul);
+void tratarJsonComando(const String &mensagem);
+void acenderLampada(bool lampadaParametro);
+
+void setup()
+{
+  pinMode(15, OUTPUT);
+
+  configurarDebug();
+  configurarLedRGB();
+  conectarWiFi();
+  configurarMQTT();
+  registrarCallBackMensagem(tratarMensagemRecebida);
+  conectarMQTT();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  garantirWiFiConectado();
+  garantirMQTTConectado();
+  loopMQTT();
+
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void tratarMensagemRecebida(const char *topico, const String &mensagem)
+{
+  debugInfo("==================================");
+  debugInfo("Mensagem recebida na aplicação");
+  debugInfo("==================================");
+
+  if (topico == nullptr)
+  {
+    debugErro("Tópico MQTT inválido");
+    return;
+  }
+
+  debugInfo("Tópico: " + String(topico));
+  debugInfo("Mensagem " + mensagem);
+
+  debugInfo(String(strcmp(topico, TOPICO_COMANDO)));
+
+  if (strcmp(topico, TOPICO_COMANDO) == 0)
+  {
+    tratarJsonComando(mensagem);
+    return;
+  }
+
+  debugErro("Tópico näo tratado: " + String(topico));
 }
+
+void configurarLedRGB()
+{
+  ledRGB.begin();
+  ledRGB.setBrightness(80); // IGUAL O ANALOG WRITE (0 a 255)
+  ledRGB.clear();
+  ledRGB.show();
+
+  debugInfo("LED RGB configurado no GPIO " + String(PINO_LED_RGB));
+}
+
+void alterarCorLedRGB(int vermelho, int verde, int azul)
+{
+  // essa constrain faz a mesma coisa que um if(vermelho < 0) vermelho = 0 e if(vermelho > 255) vermelho = 255
+  vermelho = constrain(vermelho, 0, 255);
+  verde = constrain(verde, 0, 255);
+  azul = constrain(azul, 0, 255);
+
+  // passar qual é o endereço do led (tipo pensa na fita q tenha 5 leds ele vai falar q começa no primeiro que é o 0)
+  ledRGB.setPixelColor(0, ledRGB.Color(vermelho, verde, azul));
+  ledRGB.show();
+
+  debugInfo("Cor aplicada no LED RGB");
+  debugInfo("R: " + String(vermelho));
+  debugInfo("G: " + String(verde));
+  debugInfo("B: " + String(azul));
+}
+
+void tratarJsonComando(const String &mensagem)
+{
+  JsonDocument doc;
+
+  deserializeJson(doc, mensagem);
+
+  DeserializationError erro = deserializeJson(doc, mensagem);
+
+  if (erro)
+  {
+    debugErro("Erro ao interpretar o JSON.");
+    debugErro(erro.c_str());
+    return;
+  }
+
+  if (!doc["led"].is<JsonObject>())
+  {
+    if (!doc["led"]["r"].is<int>() || !doc["led"]["g"].is<int>() || !doc["led"]["b"].is<int>())
+    {
+      debugErro("JSON Inválido. Use led.r, led.g e led.b");
+      return;
+    }
+  }
+  else
+  {
+    int vermelho = doc["led"]["r"].as<int>();
+    int verde = doc["led"]["g"].as<int>();
+    int azul = doc["led"]["b"].as<int>();
+
+    debugInfo("entrou no else para chamar alterarCorLedRGB");
+
+    alterarCorLedRGB(vermelho, verde, azul);
+  }
+  if (doc["lampada"].is<bool>())
+  {
+    bool lampada = doc["lampada"].as<bool>();
+
+    acenderLampada(lampada);
+  }
+  else
+  {
+    debugErro("Checar a conexão da lampamda com o LED");
+    debugErro("Checar erro no JSON");
+  }
+  
+}
+void acenderLampada(bool lampadaParametro)
+{
+  debugInfo("entrou aqui");
+
+  lampada = lampadaParametro;
+  digitalWrite(PINO_LAMPADA, lampada);
+  debugInfo("Estado da Lampada: " + String(lampada));
+}
+
+
+
